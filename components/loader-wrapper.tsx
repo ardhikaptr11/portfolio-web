b541314e3527fcd60084c9842b44d87262eaae9d
Ardhika <ardhikaptr11@gmail.com>
2025-06-07 16:06:47 +0700

    refactor: improve code formatting and consistency

--- components/loader-wrapper.tsx
+++ components/loader-wrapper.tsx
@@ -1,41 +1,41 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import Loader from './screen-loader';
-import { DevToolsDetector } from '@/components/devtools-detector';
+import { useEffect, useState } from "react";
+import Loader from "./screen-loader";
+import { DevToolsDetector } from "@/components/devtools-detector";
 
 interface LoaderWrapperProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const LoaderWrapper = ({ children }: { children: React.ReactNode }) => {
-  const [isCounterDone, setCounterDone] = useState(false);
-  const [animateOut, setAnimateOut] = useState(false);
-  const [isLoaded, setIsLoaded] = useState(false);
+	const [isCounterDone, setCounterDone] = useState(false);
+	const [animateOut, setAnimateOut] = useState(false);
+	const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    if (isCounterDone) {
-      const timeout = setTimeout(() => setAnimateOut(true), 500);
-      return () => clearTimeout(timeout);
-    }
-  }, [isCounterDone]);
+	useEffect(() => {
+		if (isCounterDone) {
+			const timeout = setTimeout(() => setAnimateOut(true), 500);
+			return () => clearTimeout(timeout);
+		}
+	}, [isCounterDone]);
 
-  if (!isLoaded) {
-    return (
-      <Loader
-        animateOut={animateOut}
-        onCounterDone={() => setCounterDone(true)}
-        onFinish={() => setIsLoaded(true)}
-      />
-    );
-  }
+	if (!isLoaded) {
+		return (
+			<Loader
+				animateOut={animateOut}
+				onCounterDone={() => setCounterDone(true)}
+				onFinish={() => setIsLoaded(true)}
+			/>
+		);
+	}
 
-  return (
-    <>
-      {/* <DevToolsDetector /> */}
-      {children}
-    </>
-  );
+	return (
+		<>
+			<DevToolsDetector />
+			{children}
+		</>
+	);
 };
 
 export default LoaderWrapper;
